753e7c57e0aa10cd501e8819402b121e1bc28d1b
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-04-14 15:02:48 -0500

    save button implemented

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var tasks=[];
+var removed = false;
 
 var loadTasks = function (){
   savedTasks = localStorage.getItem("tasks");
@@ -28,10 +29,11 @@ for (var i = 9; i < 18; i++){
     if(!tasks[i-9].text){
       tasks[i-9].text="";
     };
-    var timeTask = $("<div>").addClass("col-10 past description");
+    var timeTask = $("<div>").addClass("col-10 past description task");
     $(timeTask).attr("id",$(timeHour).text());
     $(timeTask).html(tasks[i-9].text);
     var timeSave = $("<button>").addClass("col-1 btn saveBtn");
+    $(timeSave).attr("id",$(timeHour).text());
     $(timeSave).html('<span class="oi oi-clipboard"></span>');
     $(timeRow).append(timeHour);
     $(timeRow).append(timeTask);
@@ -46,7 +48,7 @@ var saveTask = function(){
 };
 
 var setTask = function(id, text){
-    console.log("made it");
+    console.log(id,text);
     for (i=0; i<9; i++){
         if(tasks[i].id === id){
           tasks[i].text = text;
@@ -55,7 +57,15 @@ var setTask = function(id, text){
   saveTask();
 };
 
-$(".time-block").on("click", ".description", function(){
+$(".time-block").on("click", ".saveBtn", function(){
+    var id = $(this).attr("id");
+    var text = $(this).closest(".time-block").children(".description").html();
+    setTask(id,text);
+ });
+
+$(".time-block").on("click", ".task", function(){
+    removed = false;
+    console.log("please");
     var text = $(this).text().trim();
     var textInput = $("<textarea>").addClass("col-10 description").val(text);
     $(textInput).attr("id",$(this).attr("id"));
@@ -63,15 +73,20 @@ $(".time-block").on("click", ".description", function(){
     textInput.trigger("focus");
 });
 
+
+
 $(".time-block").on("blur", "textarea", function(){
-    
-    var text = $("<div>").addClass("col-10 description past");
+    if (removed){return};
+    removed = true;
+    var text = $("<div>").addClass("col-10 description past task");
     $(text).attr("id",$(this).attr("id"));
-    for (var i=0; i<9; i++){
-        if(tasks[i].id === $(this).attr("id")){
-          $(text).html(tasks[i].text);
-        }
-    }
-    $(this).replaceWith(text);
+    $(text).html($(this).val());
+    // for (var i=0; i<9; i++){
+    //     if(tasks[i].id === $(this).attr("id")){
+    //       $(text).html(tasks[i].text);
+    //     }
+    // }
+    var replace = $(this);
+    $(replace).replaceWith(text);
 })
 
