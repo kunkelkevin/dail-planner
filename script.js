a2982e71bcf2f107aa43073cc5d13ec1359c79a3
Kevin Kunkel <kunkelkevin@yahoo.com>
2021-04-15 17:33:47 -0500

    working locally but not on web

--- script.js
+++ script.js
@@ -45,7 +45,8 @@ var loadPage = function () {
     $(timeTask).attr("id", $(timeHour).text());
     $(timeTask).attr("time", i);
     $(timeTask).addClass(String(i));
-    $(timeTask).html(tasks[i - 9].text);
+    $(timeTask).html("tasks[i - 9].text");
+    // $(timeTask).html(tasks[i - 9].text);
     var timeSave = $("<button>").addClass("col-1 btn saveBtn");
     $(timeSave).attr("id", $(timeHour).text());
     $(timeSave).html('<span class="oi oi-clipboard"></span>');
